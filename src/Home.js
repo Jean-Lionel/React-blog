f6dc810ddee3a8bc5bb51a9e7502cb0b754d94ef
Jean-Lionel <nijeanlionel@gmail.com>
2022-03-05 07:15:29 +0200

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return ( 
+        <div className="home">
+            <h1>Voici ma page d'accueil</h1>
+        </div>
+     );
+}
+ 
+export default Home;
---
+++ src/NavBar.js
@@ -0,0 +1,21 @@
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <div>
+                <div>
+                 <a href="">Bpro Blog</a>
+                </div>
+                <ul className="liens">
+                    <li>
+                        <a href="" className="lien">Accueil</a>
+                    </li>
+                    <li>
+                    <a href="" className="lien">Creér Article</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+ 
+export default NavBar;
